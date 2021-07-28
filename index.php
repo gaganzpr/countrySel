bb12db2e701d67426d484b1f7081b11b15f44120
ShineD-173 <ShineD-173@domain.wgs.local>
2021-07-28 18:47:16 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+?>
+<div class="niceCountryInputSelector" style="width: 300px;" data-selectedcountry="US" data-showspecial="false" data-showflags="true" data-i18nall="All selected"
+    data-i18nnofilter="No selection" data-i18nfilter="Filter" data-onchangecallback="onChangeCallback" />
+</div>
